8b6a517fe4b56614f0665d3815bf64e4963c0a31
Ekaterina Mashtanova <156777319+rizetaa@users.noreply.github.com>
2026-04-18 22:37:28 +0300

    Update analysis_qc_introgression.py

--- analysis_qc_introgression.py
+++ analysis_qc_introgression.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 
 from __future__ import annotations
+from pathlib import Path
 
 import argparse
 import sys
-from pathlib import Path
-
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
